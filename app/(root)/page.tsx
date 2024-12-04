228715f74302494d4745547fd72e57560aea5bf6
Asad Abbas <khanasad92332@gmail.com>
2024-12-04 16:52:45 +0500

    work flow ready

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+////testing/////
+
+export default function Home() {
+  return (
+    <>
+    <section className="pink_container">
+      <h1 className="heading">Pitch your startup <br/> connect with entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl"> Submit Ideas, vote on Pitches, and Get Noticed in Virtual competitions</p>
+    </section>
+    </>
+  );
+}
